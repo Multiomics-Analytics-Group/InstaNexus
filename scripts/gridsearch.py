ba1568b1f6e6afc50b38149d29f4836c508414d3
Henry Webel <heweb@dtu.dk>
2025-11-20 15:12:42 +0100

    :bug: apply isort

--- scripts/gridsearch.py
+++ scripts/gridsearch.py
@@ -26,10 +26,9 @@ import os
 from concurrent.futures import ProcessPoolExecutor, as_completed
 from pathlib import Path
 
-from tqdm import tqdm
-
 from src.opt.opt_dbg import run_pipeline_dbg
 from src.opt.opt_greedy import run_pipeline_greedy
+from tqdm import tqdm
 
 BASE_DIR = Path(__file__).resolve().parents[2]
 
